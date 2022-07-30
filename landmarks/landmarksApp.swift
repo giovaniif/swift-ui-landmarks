0e3e9597e005d598b6a86ace872a742e68a2e14b
Giovani Farias <riccog.25@gmail.com>
2022-07-30 16:53:02 -0300

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Giovani Farias on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
